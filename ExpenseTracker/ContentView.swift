4fbb28daec3f37e1b1b079712518b4174ae0f790
Isaac Silva <isaackun98@gmail.com>
2024-01-26 21:47:02 +0900

    ADD: Expense Gradient View

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -11,13 +11,13 @@ struct ContentView: View {
     var body: some View {
         ZStack{
             VStack{
-                ExpenseCardView()
+                ExpenseCardView2()
                 SpendingCardView()
             }
         }
     }
     
-    // MARK: Expense Gradient View
+    // MARK: Expense Gradient View Using GeometryReactagle
     @ViewBuilder
     func ExpenseCardView()->some View{
         //DEV MARK: MAYBE REMOVE GEOMETRYREADER
@@ -77,11 +77,55 @@ struct ContentView: View {
             Text("oi")
             Text("oi")
             Text("oi")
+            Text("oi")
+            Text("oi")
+            Text("oi")
         }
         .frame(maxWidth: .infinity, alignment: .top)
     }
+    
+    // MARK: Expense Gradient View
+    @ViewBuilder
+    func ExpenseCardView2()->some View{
+    
+        VStack{
+            Text("Current Balance")
+            Text("$320,30")
+                .padding([.top, .bottom], 5)
+                .font(.system(size: 50))
+            Text("September 25")
+            
+            HStack{
+                Image(systemName: "arrow.down.left.circle.fill")
+                
+                VStack(spacing: 4){
+                    Text("INCOME")
+                    Text("250,00")
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 23)
+                
+                Image(systemName: "arrow.down.left.circle.fill")
+                
+                VStack(spacing: 4){
+                    Text("EXPENSE")
+                    Text("250,00")
+                }.padding(.top, 23)
+            }
+            .padding(.horizontal)
+            .padding(.trailing)
+            
+        }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .background(LinearGradient(colors: [.purple, .orange], startPoint: .topTrailing, endPoint: .bottomTrailing))
+            .foregroundStyle(.white)
+            .cornerRadius(25)
+            .ignoresSafeArea()
+        
+    }
+    
 }
 
 #Preview {
     ContentView()
 }
+
