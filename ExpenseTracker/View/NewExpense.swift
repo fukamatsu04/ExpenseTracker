f0bd644fc1f56beabcbffb3b6f69046443b29aa4
Isaac Silva <isaackun98@gmail.com>
2024-02-19 21:25:31 +0900

    ADD: Save button to the NewExpense View

--- ExpenseTracker/View/NewExpense.swift
+++ ExpenseTracker/View/NewExpense.swift
@@ -63,6 +63,23 @@ struct NewExpense: View {
                 }
                 .padding(.top, 25)
                 
+                //MARK: Custom label "Checkbox"
+                Label{
+                    
+                    CustomCheckboxes()
+                    
+                } icon: {
+                    Image(systemName: "arrow.up.arrow.down")
+                        .font(.title3)
+                        .foregroundStyle(Color.gray)
+                }
+                .padding(.vertical, 20)
+                .padding(.horizontal, 15)
+                .background{
+                    RoundedRectangle(cornerRadius: 12, style: .continuous).fill(.white)
+                }
+                .padding(.top, 10)
+                
                 //MARK: Custom Label "Dateset"
                 Label{
                     DatePicker.init("", selection: $expenseViewModel.date, in:Date.distantPast...Date(), displayedComponents: [.date])
@@ -79,13 +96,32 @@ struct NewExpense: View {
                 .background{
                     RoundedRectangle(cornerRadius: 12, style: .continuous).fill(.white)
                 }
-                .padding(.top, 25)
+                .padding(.top, 10)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            // MARK: Save Button
+            Button {
+                
+            }label: {
+                Text("Save")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding(.vertical, 15)
+                    .frame(maxWidth: .infinity)
+                    .opacity(0.7)
+                    .background{
+                        RoundedRectangle(cornerRadius: 12, style: .continuous)
+                            .fill(.white)
+                    }
+                    .foregroundStyle(.black)
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
         .background(LinearGradient(colors: [.purple, .orange], startPoint: .topTrailing, endPoint: .bottomTrailing).ignoresSafeArea())
         .overlay(alignment: .topTrailing){
-            
+        
             //MARK: Close Button
             Button{
                 env.dismiss()
@@ -96,6 +132,39 @@ struct NewExpense: View {
                     .opacity(0.7)
             }
         }
+    }
+    
+    //MARK: Custom Checkbox
+    @ViewBuilder
+    func CustomCheckboxes()-> some View{
+        HStack(spacing: 10){
+            ForEach([ExpenseType.income, ExpenseType.expense], id: \.self){type in
+                ZStack{
+                    RoundedRectangle(cornerRadius: 2)
+                        .stroke(.black,lineWidth: 2)
+                        .opacity(0.5)
+                        .frame(width: 20, height: 20)
+                    
+                    if expenseViewModel.type == type{
+                        Image(systemName: "checkmark")
+                            .font(.caption.bold())
+                            .foregroundStyle(.green)
+                    }
+                }
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    expenseViewModel.type = type
+                }
+                
+                Text(type.rawValue.capitalized)
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .opacity(0.7)
+                    .padding(.trailing, 10)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.leading, 10)
     }
 }
 
